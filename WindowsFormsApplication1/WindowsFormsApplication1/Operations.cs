b577eb0f88edc018afc54a2dddffd62e09bda574
Ginko-san <21salchi21@gmail.com>
2015-05-02 10:59:51 -0600

    en proceso

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Operations.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    class Operations
+    {
+
+        /// Rainbow filter try
+
+        public static Bitmap RainbowFilter(Bitmap bmp)
+        {
+
+            Bitmap temp = new Bitmap(bmp.Width, bmp.Height);
+            int raz = bmp.Height / 4;
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int x = 0; x < bmp.Height; x++)
+                {
+
+                    if (i < (raz))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B));
+                    }
+                    else if (i < (raz * 2))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R, bmp.GetPixel(i, x).G / 5, bmp.GetPixel(i, x).B));
+                    }
+                    else if (i < (raz * 3))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B / 5));
+                    }
+                    else if (i < (raz * 4))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B / 5));
+                    }
+                    else
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G / 5, bmp.GetPixel(i, x).B / 5));
+                    }
+                }
+
+            }
+            return temp;
+        }
+
+
+        public static Bitmap RainbowFilterParallel(Bitmap bmp)
+        {
+
+            Bitmap temp = new Bitmap(bmp.Width, bmp.Height);
+            int raz = bmp.Height / 4;
+
+            Parallel.For(0, bmp.Width, i =>
+            {
+                Parallel.For(0, bmp.Height, x =>
+                {
+
+                    if (i < (raz))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B));
+                    }
+                    else if (i < (raz * 2))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R, bmp.GetPixel(i, x).G / 5, bmp.GetPixel(i, x).B));
+                    }
+                    else if (i < (raz * 3))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B / 5));
+                    }
+                    else if (i < (raz * 4))
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G, bmp.GetPixel(i, x).B / 5));
+                    }
+                    else
+                    {
+                        temp.SetPixel(i, x, Color.FromArgb(bmp.GetPixel(i, x).R / 5, bmp.GetPixel(i, x).G / 5, bmp.GetPixel(i, x).B / 5));
+                    }
+
+                });
+                
+            });
+            return temp;
+        }
+
+
+    }
+}
